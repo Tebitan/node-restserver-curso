2f6204d063f4c0a1e99d5938ef9ec76d6666be6e
Esteban <esteban.navas01@gmail.com>
2020-10-26 07:50:42 -0500

    Variable de enotno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -16,6 +16,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://tebitan:TLey9bBqntm4DJgj@cluster0.yte8v.mongodb.net/cafe';
+    urlDB = process.env.MOGO_URI;
 }
 process.env.URLDB = urlDB;
